cb42b9ff1417e1a793498381b56438c45580bbd4
Roger Rangel <roger.rangel.ee@gmail.com>
2022-04-26 11:07:00 +0200

    Entering the Lottery

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
     manager: "",
     players: [],
     balance: "",
-    value: ""
+    value: "",
+    message: ""
   }
 
   async componentDidMount() {
@@ -23,10 +24,15 @@ class App extends React.Component {
     event.preventDefault();
 
     const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'})
+
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(this.state.value, 'ether')
     })
+
+    this.setState({message: 'You have been entered!'})
   }
 
   render() {
@@ -52,6 +58,8 @@ class App extends React.Component {
           </div>
           <button>Enter</button>
         </form>
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
