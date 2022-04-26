28b70f1d6a87da83a1857f9dc0e3a6e244178619
Roger Rangel <roger.rangel.ee@gmail.com>
2022-04-26 11:17:58 +0200

    Picking a Winner

--- src/App.js
+++ src/App.js
@@ -35,6 +35,18 @@ class App extends React.Component {
     this.setState({message: 'You have been entered!'})
   }
 
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({message: "Waiting on transaction success..."})
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+
+    this.setState({message: "A winner has been picked!"})
+  }
+
   render() {
     
     return (
@@ -58,7 +70,12 @@ class App extends React.Component {
           </div>
           <button>Enter</button>
         </form>
+
         <hr />
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a Winner!</button>
+        <hr />
+
         <h1>{this.state.message}</h1>
       </div>
     );
